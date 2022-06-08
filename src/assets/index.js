ae971a450aa2fc5f86e5d7d8fb57d5c40885fdd6
Alec Dewulf <alec@cisudbury.ca>
2022-06-08 15:22:55 -0400

    images and logo

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+import Cancer1 from "../assets/cancer_case/2100011202/p2100011202ml.jpeg";
+import Cancer2 from "../assets/cancer_case/2100011202/p2100011202mr.jpeg";
+
+// import Normal1 from "../assets/normal_cases/2100028602/p2100028602ml.jpeg";
+// import Normal2 from "../assets/normal_cases/2100028602/p2100028602mr.jpeg";
+// import Normal3 from "../assets/normal_cases/2100054800/p2100054800ml.jpeg";
+// import Normal4 from "../assets/normal_cases/2100054800/p2100054800mr.jpeg";
+// import Normal5 from "../assets/normal_cases/2100060904/p2100060904ml.jpeg";
+// import Normal6 from "../assets/normal_cases/2100060904/p2100060904mr.jpeg";
+
